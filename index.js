2d00541275ba0a86fb6407deffd92425bccd9e29
MEBonham <programmer@mckaybonham.com>
2018-11-09 14:09:00 -0700

    GET and POST functional (except 404 errors)

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dbAction = require("./data/helpers/actionModel.js");
 
 const server = express();
 
+server.use(express.json());
+
 const port = 9000;
 
 server.get("/", (req, res) => {
@@ -53,23 +55,43 @@ server.get("/api/actions/:id", (req, res) => {
 });
 
 server.post("/api/projects", (req, res) => {
-    console.log(req.body);
     let projData = req.body;
-    // if (!projData.name || !projData.description) {
-    //     res.status(400).json({ error: "Please provide a name and description." });
-    // } else {
-    //     projData = {
-    //         completed: false,
-    //         ...projData
-    //     };
-        dbProject.insert(projData)
+    if (!projData.name || !projData.description) {
+        res.status(400).json({ error: "Please provide a name and description." });
+    } else {
+        newProject = {
+            completed: false,
+            ...projData
+        };
+        dbProject.insert(newProject)
             .then(project => {
                 res.status(200).json(project);
             })
             .catch(err => {
                 res.status(500).json({ error: "The Project could not be added." });
             });
-    // }
+    }
+});
+
+server.post("/api/actions", (req, res) => {
+    let actData = req.body;
+    if (!actData.project_id || !actData.description || !actData.notes) {
+        res.status(400).json({ error: "Please provide a description, notes, and a project ID." });
+    } else if (actData.description.length > 128) {
+        res.status(400).json({ error: "Description is limited to 128 characters." });
+    } else {
+        newAction = {
+            completed: false,
+            ...actData
+        };
+        dbAction.insert(newAction)
+            .then(action => {
+                res.status(200).json(action);
+            })
+            .catch(err => {
+                res.status(500).json({ error: "The Action could not be added." });
+            });
+    }
 });
 
 server.listen(port, () => console.log(`API running on port ${port}`));
