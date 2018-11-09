c9d436daf167c4da6e07fb58c55d366dcb2c7216
MEBonham <programmer@mckaybonham.com>
2018-11-09 10:35:07 -0700

    No response from localhost:9000 - why?

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const server = require("./api/server.js");
+const express = require("express");
+
+const serverFile = require("./api/server.js");
+const dbProject = require("./data/helpers/projectModel.js");
+const dbAction = require("./data/helpers/actionModel.js");
+
+const server = express();
 
 const port = 9000;
 
-server.listen(port, () => console.log(`API running on port ${port}`));
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.json("alive");
+});
+
+serverFile.listen(port, () => console.log(`API running on port ${port}`));
