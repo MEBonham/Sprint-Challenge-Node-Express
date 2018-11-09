db894dd4759f9633ecf2295c8af1bcf44f3585d5
MEBonham <programmer@mckaybonham.com>
2018-11-09 11:06:47 -0700

    Consolidated code in index.js, not server.js

--- api/server.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    listen: (port, cb) => {
-        console.log("server is alive");
-        cb();
-    }
-};
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-const serverFile = require("./api/server.js");
 const dbProject = require("./data/helpers/projectModel.js");
 const dbAction = require("./data/helpers/actionModel.js");
 
@@ -8,10 +7,8 @@ const server = express();
 
 const port = 9000;
 
-server.use(express.json());
-
 server.get("/", (req, res) => {
     res.json("alive");
 });
 
-serverFile.listen(port, () => console.log(`API running on port ${port}`));
+server.listen(port, () => console.log(`API running on port ${port}`));
